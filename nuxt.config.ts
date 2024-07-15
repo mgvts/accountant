0e73f89f67568df1f2853c7c62c80a0bd4256c19
mgvts <89970331+mgvts@users.noreply.github.com>
2024-07-15 21:22:44 +0300

    fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/accountant/'
+    },
     compatibilityDate: '2024-07-14',
     devtools: {enabled: true},
 
